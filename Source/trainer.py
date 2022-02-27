d9296c663b60a4964b115f36733d9c6ebbb1e810
Tristansang <tristansang001@gmail.com>
2022-02-27 16:12:28 +0200

    We added more words from our word list (Vocu)

--- Source/trainer.py
+++ Source/trainer.py
@@ -6,9 +6,23 @@ else:
     print ("Sorry, but the answer is wrong! Ask Tristan, he knows!")
 
 
-input_from_trainee = input("What is blue in shona?")
+input_from_trainee = input("What is white in shona?")
 
-if input_from_trainee == "bru":
+if input_from_trainee == "jena":
+    print ("yoo !!! yooo!!!")
+else:
+    print ("Sorry, but the answer is wrong! Ask Tristan, he knows!")
+
+input_from_trainee = input("What is black in shona?")
+
+if input_from_trainee == "tema":
+    print ("yoo !!! yooo!!!")
+else:
+    print ("Sorry, but the answer is wrong! Ask Tristan, he knows!")
+
+input_from_trainee = input("What is human in shona?")
+
+if input_from_trainee == "munhu":
     print ("yoo !!! yooo!!!")
 else:
     print ("Sorry, but the answer is wrong! Ask Tristan, he knows!")
